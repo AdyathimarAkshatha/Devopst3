a61bb06ff3513dfd725bc7ff40b9b1c72e97689b
AdyathimarAkshatha <98754593+AdyathimarAkshatha@users.noreply.github.com>
2022-03-16 20:03:14 +0000

    Add files via upload

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.77.0"
+    }
+  }
+  backend "azurerm" {
+    
+  }   
+
+}
+
+provider "azurerm" {
+  features {}
+}
